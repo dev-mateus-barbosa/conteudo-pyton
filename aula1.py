aeb53f336a8b7d6ef6f5e4d5253b61b9c55a767c
MATEUS BARBOSA <mateus_barbosa150@estudante.sesisenai.org.br>
2025-10-03 09:50:10 -0300

    aula1: comando print

---
+++ aula1.py
@@ -0,0 +1,8 @@
+#escapei da madiçao
+from traceback import print_exc
+
+print("ola mundo")
+print("eu sou hacker")
+print("esse e o curso de:",end=" ")
+print("logica","de","programaçao")
+print("frase","com","hifen",sep="-")
