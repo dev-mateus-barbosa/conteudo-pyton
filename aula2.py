3127133e62a0e2650ed661e085e05b3585460303
MATEUS BARBOSA <mateus_barbosa150@estudante.sesisenai.org.br>
2025-10-03 11:18:21 -0300

    aula 2 :variaveis e tipo

---
+++ aula2.py
@@ -0,0 +1,25 @@
+#trabalhando com variaveis
+from pprint import pprint
+
+#definindo uma varialvel com meu nome
+nome = "Mateus"
+print("o nome e",nome)
+palavra = "XULOFINFOLA"
+letra ="A"
+quantidade =10
+valor = 10.5
+aberto = False
+
+print("o valor de palavra e",palavra,end="")
+print("(tipo: ",type(palavra),")")
+
+print("o valor de letra e", letra)
+print(" o valor :", type(letra),"")
+
+print("o valor de quantidade e",quantidade)
+print("o valor:",type(quantidade),")")
+
+print("o valor de",valor)
+
+
+print("o valor de aberto e",aberto)
