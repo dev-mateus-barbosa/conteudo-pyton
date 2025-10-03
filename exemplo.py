544e47c46dd982a0764d4df653b660fef75ba189
MATEUS BARBOSA <mateus_barbosa150@estudante.sesisenai.org.br>
2025-10-03 11:24:24 -0300

    aula 2

---
+++ exemplo.py
@@ -0,0 +1,10 @@
+nome= input("digite seu nome")
+print("o nome digitado e",nome)
+print("ola,")
+quantidade =int(input("digite a quantidade de pessoas na sala"))
+print("ha",quantidade,"pessoas na sala",type(quantidade))
+
+outra - turma =20
+soma = (quantidade + outra_turma)
+print('a soma e',soma)
+
